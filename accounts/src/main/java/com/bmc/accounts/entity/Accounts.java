46593521c99f9593dab6f1a16e4738c776996e19
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2024-08-30 23:39:13 -0700

    Add entities

---
+++ accounts/src/main/java/com/bmc/accounts/entity/Accounts.java
@@ -0,0 +1,32 @@
+package com.bmc.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Accounts extends BaseEntity{
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "account_Type")
+    private String accountType;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+
+}
---
+++ accounts/src/main/java/com/bmc/accounts/entity/Customer.java
@@ -0,0 +1,31 @@
+package com.bmc.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer extends BaseEntity {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+////    Generate primary key based on native style of database
+//    @GenericGenerator(name = "native", strategy = "native")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+
+}
