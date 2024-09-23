8e788cf506f8cb1e3fa64bb85487ee9c4e9934a2
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2024-09-23 11:47:30 +0330

    Add build and contact informations as services

--- cards/src/main/java/com/bmc/cards/CardsApplication.java
+++ cards/src/main/java/com/bmc/cards/CardsApplication.java
@@ -1,10 +1,13 @@
 package com.bmc.cards;
 
+import com.bmc.cards.dto.CardsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = CardsContactInfoDto.class)
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class CardsApplication {
 
--- cards/src/main/java/com/bmc/cards/controller/CardsController.java
+++ cards/src/main/java/com/bmc/cards/controller/CardsController.java
@@ -1,6 +1,7 @@
 package com.bmc.cards.controller;
 
 import com.bmc.cards.constants.CardsConstants;
+import com.bmc.cards.dto.CardsContactInfoDto;
 import com.bmc.cards.dto.CardsDto;
 import com.bmc.cards.dto.ErrorResponseDto;
 import com.bmc.cards.dto.ResponseDto;
@@ -13,7 +14,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -26,12 +27,21 @@ import org.springframework.web.bind.annotation.*;
 )
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
 public class CardsController {
 
 
-    private ICardsService iCardsService;
+    private final ICardsService iCardsService;
+
+    private final CardsContactInfoDto cardsContactInfoDto;
+
+    @Value("build.version")
+    private String buildVersion;
+
+    public CardsController(ICardsService iCardsService, CardsContactInfoDto cardsContactInfoDto) {
+        this.iCardsService = iCardsService;
+        this.cardsContactInfoDto = cardsContactInfoDto;
+    }
 
     @Operation(
             summary = "Create Card REST API",
@@ -160,5 +170,54 @@ public class CardsController {
         }
     }
 
+
+    @Operation(
+            summary = "Get build information",
+            description = "Get build information that is deployed into customer cards"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(buildVersion);
+    }
+
+    @Operation(
+            summary = "Get Contact Info",
+            description = "Contact Info details that can be reached out in case of any issues"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<CardsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cardsContactInfoDto);
+    }
 
 }
